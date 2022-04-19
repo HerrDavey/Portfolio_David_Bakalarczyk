cb11bb76cb1695892973481fe0c38a4055cb4a41
David Bakalarczyk <david.bakalarczyk00@gmail.com>
2022-04-19 21:41:30 +0200

    Update 19.04.2022

---
+++ 11. Szablony danych, konwertery i szablony kontrolek/value_converters/ColorPltoColorEnConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace value_converters
+{
+    class ColorPltoColorEnConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if(targetType != typeof(Brush))
+            {
+                throw new InvalidOperationException("Celem powinien być typ Brush!");
+            }
+            string kolorPL = value.ToString();
+            Dictionary<string, Brush> kolory = new Dictionary<string, Brush>();
+
+            kolory.Add("Czarny", Brushes.Black);
+            kolory.Add("Czerwony", Brushes.Red);
+            kolory.Add("Żółty", Brushes.Yellow);
+            kolory.Add("Zielony", Brushes.Green);
+            kolory.Add("Niebieski", Brushes.Blue);
+
+            if (kolory.ContainsKey(kolorPL))
+                return kolory[kolorPL];
+            else
+                return Brushes.LightGray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
